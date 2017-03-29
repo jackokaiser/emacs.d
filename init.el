3c7ec0bbcf3c72502ce75e4cbe6415bc0f72ab07
Jacques KAISER <jkaiser@fzi.de>
2017-03-29 16:50:02 +0200

    running emacs from virtualenv

--- init.el
+++ init.el
@@ -375,6 +375,18 @@
 ;;;;;;;;;;;;;;;;; PYTHON CONF ;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'virtualenvwrapper)
+(venv-initialize-interactive-shells) ;; if you want interactive shell support
+(venv-initialize-eshell) ;; if you want eshell support
+;; note that setting `venv-location` is not necessary if you
+;; use the default location (`~/.virtualenvs`), or if the
+;; the environment variable `WORKON_HOME` points to the right place
+(setq venv-location "~/.virtualenvs/")
+(venv-workon "emacs")
+
+(add-to-list 'package-archives
+             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+(elpy-enable)
 
 (defun python-add-breakpoint ()
   "Add a break point"
