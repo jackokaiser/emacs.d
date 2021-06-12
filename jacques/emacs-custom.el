543bd072e6913b5d3bee76776ca28deac8927277
Jacques KAISER <jackokaiser@gmail.com>
2021-06-12 12:09:53 +0200

    ignore more python check errors

--- jacques/emacs-custom.el
+++ jacques/emacs-custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(flycheck-pycheckers-checkers '(pylint mypy3))
  '(flycheck-pycheckers-ignore-codes
-	 '("C0411" "C0413" "C0103" "C0111" "W0142" "W0201" "W0232" "W0403" "W0511" "E1002" "E1101" "E1103" "R0201" "R0801" "R0903" "R0904" "R0914" "R0913"))
+	 '("C0411" "C0413" "C0103" "C0111" "W0142" "W0201" "W0232" "W0403" "W0511" "E1002" "E1101" "E1103" "R0201" "R0801" "R0903" "R0904" "R0914" "R0913" "W1203"))
  '(package-selected-packages
 	 '(jedi-direx jedi yasnippet-classic-snippets flycheck flycheck-pycheckers web-mode markdown-preview-eww virtualenv virtualenvwrapper py-autopep8 dockerfile-mode pyenv-mode conda py-yapf cmake-mode typescript-mode ox-mediawiki yaml-mode haskell-mode magit yasnippet-snippets browse-kill-ring yasnippet markdown-mode idle-highlight-mode highlight-indent-guides auto-complete company ##)))
 (custom-set-faces
